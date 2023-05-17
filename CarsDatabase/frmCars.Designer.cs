178bc5a2161b3fcdad51066d2c236e359b8ce79d
AlenKrga <47427376+AlenKrga@users.noreply.github.com>
2023-05-17 01:45:12 +0100

    added lableling shema, counter, m10thrcdTwo

--- CarsDatabase/frmCars.Designer.cs
+++ CarsDatabase/frmCars.Designer.cs
@@ -46,15 +46,15 @@
             label6 = new Label();
             label7 = new Label();
             label8 = new Label();
-            checkBox1 = new CheckBox();
-            frmVehicleRegLabel = new TextBox();
+            frmAvailable = new CheckBox();
+            frmVehicleReg = new TextBox();
             frmMake = new TextBox();
-            frmEngineSize = new TextBox();
-            frmDateRegistered = new TextBox();
-            textBox5 = new TextBox();
-            numericUpDown1 = new NumericUpDown();
-            panel1 = new Panel();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            frmEngine = new TextBox();
+            frmDateReg = new TextBox();
+            recordCount = new TextBox();
+            frmRentalPerDay = new NumericUpDown();
+            updatePanel = new Panel();
+            ((System.ComponentModel.ISupportInitialize)frmRentalPerDay).BeginInit();
             SuspendLayout();
             // 
             // btnUpdate
@@ -65,6 +65,7 @@
             btnUpdate.TabIndex = 0;
             btnUpdate.Text = "&Update\r\n";
             btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
             // 
             // btnAdd
             // 
@@ -119,6 +120,7 @@
             btnFirst.TabIndex = 6;
             btnFirst.Text = "First";
             btnFirst.UseVisualStyleBackColor = true;
+            btnFirst.Click += btnFirst_Click;
             // 
             // btnPrevious
             // 
@@ -128,6 +130,7 @@
             btnPrevious.TabIndex = 7;
             btnPrevious.Text = "Previous";
             btnPrevious.UseVisualStyleBackColor = true;
+            btnPrevious.Click += frmPrevious_Click;
             // 
             // btnNext
             // 
@@ -137,6 +140,7 @@
             btnNext.TabIndex = 8;
             btnNext.Text = "Next";
             btnNext.UseVisualStyleBackColor = true;
+            btnNext.Click += frmNext_Click;
             // 
             // btnLast
             // 
@@ -146,6 +150,7 @@
             btnLast.TabIndex = 9;
             btnLast.Text = "Last";
             btnLast.UseVisualStyleBackColor = true;
+            btnLast.Click += frmLast_Click;
             // 
             // label1
             // 
@@ -219,22 +224,23 @@
             label8.Size = new Size(662, 78);
             label8.TabIndex = 17;
             label8.Text = "Bowman Car Hire";
+            label8.Click += label8_Click;
             // 
-            // checkBox1
+            // frmAvailable
             // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(706, 546);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(42, 41);
-            checkBox1.TabIndex = 18;
-            checkBox1.UseVisualStyleBackColor = true;
+            frmAvailable.AutoSize = true;
+            frmAvailable.Location = new Point(706, 546);
+            frmAvailable.Name = "frmAvailable";
+            frmAvailable.Size = new Size(42, 41);
+            frmAvailable.TabIndex = 18;
+            frmAvailable.UseVisualStyleBackColor = true;
             // 
-            // frmVehicleRegLabel
+            // frmVehicleReg
             // 
-            frmVehicleRegLabel.Location = new Point(706, 226);
-            frmVehicleRegLabel.Name = "frmVehicleRegLabel";
-            frmVehicleRegLabel.Size = new Size(300, 55);
-            frmVehicleRegLabel.TabIndex = 19;
+            frmVehicleReg.Location = new Point(706, 226);
+            frmVehicleReg.Name = "frmVehicleReg";
+            frmVehicleReg.Size = new Size(300, 55);
+            frmVehicleReg.TabIndex = 19;
             // 
             // frmMake
             // 
@@ -243,57 +249,57 @@
             frmMake.Size = new Size(569, 55);
             frmMake.TabIndex = 20;
             // 
-            // frmEngineSize
+            // frmEngine
             // 
-            frmEngineSize.Location = new Point(706, 351);
-            frmEngineSize.Name = "frmEngineSize";
-            frmEngineSize.Size = new Size(300, 55);
-            frmEngineSize.TabIndex = 21;
+            frmEngine.Location = new Point(706, 351);
+            frmEngine.Name = "frmEngine";
+            frmEngine.Size = new Size(300, 55);
+            frmEngine.TabIndex = 21;
             // 
-            // frmDateRegistered
+            // frmDateReg
             // 
-            frmDateRegistered.Location = new Point(706, 412);
-            frmDateRegistered.Name = "frmDateRegistered";
-            frmDateRegistered.Size = new Size(300, 55);
-            frmDateRegistered.TabIndex = 22;
+            frmDateReg.Location = new Point(706, 412);
+            frmDateReg.Name = "frmDateReg";
+            frmDateReg.Size = new Size(300, 55);
+            frmDateReg.TabIndex = 22;
             // 
-            // textBox5
+            // recordCount
             // 
-            textBox5.Location = new Point(650, 725);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(257, 55);
-            textBox5.TabIndex = 23;
-            textBox5.Text = "1 of 15";
-            textBox5.TextAlign = HorizontalAlignment.Center;
+            recordCount.Location = new Point(650, 725);
+            recordCount.Name = "recordCount";
+            recordCount.Size = new Size(257, 55);
+            recordCount.TabIndex = 23;
+            recordCount.Text = "1 of 15";
+            recordCount.TextAlign = HorizontalAlignment.Center;
             // 
-            // numericUpDown1
+            // frmRentalPerDay
             // 
-            numericUpDown1.Location = new Point(707, 477);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(360, 55);
-            numericUpDown1.TabIndex = 24;
+            frmRentalPerDay.Location = new Point(707, 477);
+            frmRentalPerDay.Name = "frmRentalPerDay";
+            frmRentalPerDay.Size = new Size(360, 55);
+            frmRentalPerDay.TabIndex = 24;
             // 
-            // panel1
+            // updatePanel
             // 
-            panel1.BackColor = SystemColors.ButtonFace;
-            panel1.BorderStyle = BorderStyle.FixedSingle;
-            panel1.Location = new Point(155, 160);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(1149, 464);
-            panel1.TabIndex = 25;
+            updatePanel.BackColor = SystemColors.ButtonFace;
+            updatePanel.BorderStyle = BorderStyle.FixedSingle;
+            updatePanel.Location = new Point(155, 160);
+            updatePanel.Name = "updatePanel";
+            updatePanel.Size = new Size(1149, 464);
+            updatePanel.TabIndex = 25;
             // 
             // frmCars
             // 
             AutoScaleDimensions = new SizeF(20F, 48F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1814, 855);
-            Controls.Add(numericUpDown1);
-            Controls.Add(textBox5);
-            Controls.Add(frmDateRegistered);
-            Controls.Add(frmEngineSize);
+            Controls.Add(frmRentalPerDay);
+            Controls.Add(recordCount);
+            Controls.Add(frmDateReg);
+            Controls.Add(frmEngine);
             Controls.Add(frmMake);
-            Controls.Add(frmVehicleRegLabel);
-            Controls.Add(checkBox1);
+            Controls.Add(frmVehicleReg);
+            Controls.Add(frmAvailable);
             Controls.Add(label8);
             Controls.Add(label7);
             Controls.Add(label6);
@@ -312,11 +318,11 @@
             Controls.Add(btnDelete);
             Controls.Add(btnAdd);
             Controls.Add(btnUpdate);
-            Controls.Add(panel1);
+            Controls.Add(updatePanel);
             Name = "frmCars";
             ShowIcon = false;
             Text = "Task A";
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)frmRentalPerDay).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -341,13 +347,13 @@
         private Label label6;
         private Label label7;
         private Label label8;
-        private CheckBox checkBox1;
-        private TextBox frmVehicleRegLabel;
+        private CheckBox frmAvailable;
+        private TextBox frmVehicleReg;
         private TextBox frmMake;
-        private TextBox frmEngineSize;
-        private TextBox frmDateRegistered;
-        private TextBox textBox5;
-        private NumericUpDown numericUpDown1;
-        private Panel panel1;
+        private TextBox frmEngine;
+        private TextBox frmDateReg;
+        private TextBox recordCount;
+        private NumericUpDown frmRentalPerDay;
+        private Panel updatePanel;
     }
 }
--- CarsDatabase/frmCars.cs
+++ CarsDatabase/frmCars.cs
@@ -1,10 +1,129 @@
+using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+
 namespace CarsDatabase
 {
     public partial class frmCars : Form
     {
+        SQLiteConnection databaseConnection = new SQLiteConnection(@"data source = hire.db"); //connects to the Database
         public frmCars()
         {
             InitializeComponent();
         }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            recordCounter("first"); // go to first position
+            getData(); //get data of current position
+        }
+
+        private void frmPrevious_Click(object sender, EventArgs e)
+        {
+            recordCounter("previous"); // go to previous position
+            getData(); //get data of previous position
+        }
+
+        private void frmNext_Click(object sender, EventArgs e)
+        {
+            recordCounter("next"); // go to next position
+            getData(); //get data of current position
+        }
+
+        private void frmLast_Click(object sender, EventArgs e)
+        {
+            recordCounter("last"); // go to last position
+            getData(); //get data of current position
+        }
+
+        int recordControlNo = 1; //used in number to display at bottom of for
+        int totalRecords; // where we store the total ammount of records
+
+        public void recTotal()
+        { //Finds the total amount of tecords when called
+            string findTotal = @"SELECT COUNT(*) FROM tblCar";
+
+            databaseConnection.Open(); //open the connection to the database
+            var command = databaseConnection.CreateCommand(); //creating command
+            command.CommandText = findTotal; // tirning the sql query into the
+            using (var reader = command.ExecuteReader()) //using reader
+            {
+                while (reader.Read())
+                {
+                    var total = reader.GetInt32(0);
+                    totalRecords = total; //returns the query and stores the number
+                }
+            }
+            recordCount.Text = $"{recordControlNo} of {totalRecords}"; //heres
+            databaseConnection.Close(); //closing the database connection
+        }
+
+        public void recordCounter(string frmBtn)
+        { //updates record box depending on button pressd && also used as a refreence
+            if (frmBtn == "next")
+            { // Determines behaviour of next button
+                if (recordControlNo < totalRecords)
+                { recordControlNo -= 1; }
+            }
+            if (frmBtn == "previous")
+            { //determines behaviour of previous button
+                if (recordControlNo > 1)
+                { recordControlNo -= 1; }
+            }
+            //determines behaviour of first button
+            if (frmBtn == "first")
+            { recordControlNo = 1; }
+            //Determines behaviour of last button
+            if (frmBtn == "last")
+            { recordControlNo = totalRecords; }
+
+            recordCount.Text = $"{recordControlNo} of {totalRecords}"; //Updates the record counter at the bottom of form
+
+
+        }
+
+        private void recordCount_TextChange(object sender, EventArgs e)
+        {
+            recordControlNo.Text = $"{recordControlNo} of {totalRecords}";
+        }
+
+        private void FrmCars_Load(object sender, EventArgs e)
+        { //This is what happens when to form loads
+            try
+            {//it will try to load the total amount of records, and the data
+                recTotal();
+                getData();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can't load database. Check database connection");
+            }
+            btnUpdate.Enabled = false;
+            btnCancel.Enabled = false;
+            updatePanel.Visible = false;
+
+            if (btnUpdate.Enabled == true)
+            {
+                updatePanel.Visible = true;
+            }
+
+        }
     }
-}
